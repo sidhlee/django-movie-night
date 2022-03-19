6f309beabaedcf99be806b0f1d3006ebd553b2f1
Sid Hayoun Lee <sidhlee@gmail.com>
2022-03-19 11:02:50 -0400

    get movies data from api

--- movie_night/movie/views.py
+++ movie_night/movie/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def _get_movies(search_string="time"):
+    """
+    get 10 movies with the given search string.
+    TODO: add pagination option
+    """
+    api_key = "2610afcc"
+    movies_response = requests.get(
+        f"https://omdbapi.com/?s={search_string}&apikey={api_key}"
+    )
+    movies_dict = movies_response.json()
+    movies_list = movies_dict.get("Search", [])
+    return movies_list
 
 
 def index(request):
-    ''' 
+    """
     Renders html inside templates folder
-    '''
+    """
+    context = {}
+    movies = _get_movies()
+    context["newsdata"] = movies
+    print(movies)
 
-    context = {
-
-    }
-    return render(request, 'movies/index.html', context)
+    return render(request, "movies/index.html", context)
